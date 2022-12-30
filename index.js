df762968d9cd1122307d6cbe80f21861da37bc5e
faheemk23 <faheemwork23@gmail.com>
2022-12-30 16:31:39 +0530

    feat: welcome message

--- index.js
+++ index.js
@@ -38,3 +38,11 @@ var questions = [
     answer: "riddler"
   }
 ]
+
+// welcome function
+
+function welcome() {
+  var usrName = readlineSync.question("What's your name? ")
+  console.log("Hello " + usrName + ", welcome to Quiz on Batman!")
+  console.log("----------------------------------------")
+}
