4141a09c25516e12142b6774646229cbd6499122
faheemk23 <faheemwork23@gmail.com>
2022-12-30 16:30:16 +0530

    feat: data about high scores

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 var readlineSync = require('readline-sync')
 
 var score = 0;
+
+// data of high scores
+var highScore = [
+  {
+    name: "Himanshu",
+    score: 4
+  },
+  {
+    name: "Divya",
+    score: 3
+  }
+]
