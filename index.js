47f73f4c494eb84ef40a4938010628e5516f5e8d
faheemk23 <faheemwork23@gmail.com>
2023-01-06 22:17:25 +0530

    feat: chalk,  high score update functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var readlineSync = require('readline-sync')
+var chalk = require("chalk");
+var readlineSync = require('readline-sync');
 
 var score = 0;
 
@@ -6,45 +7,70 @@ var score = 0;
 var highScore = [
   {
     name: "Himanshu",
-    score: 4
+    score: 6
   },
   {
     name: "Divya",
-    score: 3
+    score: 5
+  },
+  {
+    name: "Jane",
+    score: 4
+  },
+  {
+    name: "Kaushal",
+    score: 4
   }
 ]
+
 
 // array of objects
 
 var questions = [
   {
-    question: "What is Batman's real name? ",
-    answer: "Bruce wayne"
+    question: "1) What super villain once broke Batman's back, leaving him crippled and wheelchair-bound? ",
+    answerList: ["JOKER", "KILLER CROC", "RA'S AL GHUL", "BANE"]
   },
   {
-    question: "Who took care of him after his parents died? ",
-    answer: 'alfred'
+    question: "2) What were the names of Bruce Wayne's parents? ",
+    answerList: ["GEORGE & ELAINE", "JAMES & ELIZABETH", "WAYNE & ALICE", "THOMAS & MARTHA"]
   },
   {
-    question: "What is the name of his car? ",
-    answer: 'batmobile'
+    question: "3) Who is credited with creating Batman? ",
+    answerList: ["JACK KIRBY", "JOE SHUSTER", "JERRY SIEGEL", "BOB KANE"]
   },
   {
-    question: "Does he have Superpowers? ",
-    answer: 'No'
+    question: "4) What Batman villain formerly worked as a zoologist? ",
+    answerList: ["THE PENGUIN", "POISON IVY", "KILLER CROC", "MANBAT"]
   },
   {
-    question: "What is the name of the villian that uses puzzles a lot? ",
-    answer: "riddler"
+    question: "5) What year was the character of Robin first introduced? ",
+    answerList: ["1960", "1950", "1970", "1940"]
+  },
+  {
+    question: "6) In Detective Comics #267, a magical Kobold from another dimension decides to help Batman fight crime--what is his name? ",
+    answerList: ["MR. MXYZTPLK", "BAT-MITE", "MARTIAN MANHUNTER", "BAT-DWARF"]
+  },
+  {
+    question: "7) The original Batgirl was related to what familiar Batman character? ",
+    answerList: ["THE JOKER", "BATWOMAN", "ALFRED", "COMMISSIONER GORDON"]
+  },
+  {
+    question: "8) Which of these Bat-villains was introduced first? ",
+    answerList: ["CATWOMAN", "THE PENGUIN", "THE RIDDLER", "MR. FREEZE"]
   }
 ]
+
+answersIndex = [3, 3, 3, 3, 3, 1, 1, 0]
+
+
+var usrName = readlineSync.question("What's your name? ")
 
 // welcome function
 
 function welcome() {
-  var usrName = readlineSync.question("What's your name? ")
   console.log("")
-  console.log("Hello " + usrName + ", welcome to Quiz on Batman!")
+  console.log("Hello " + chalk.blue(usrName) + ", welcome to Quiz on Batman!")
   console.log("")
   console.log("Note: The answers are case-insensitive.")
   console.log("----------------------------------------")
@@ -52,19 +78,17 @@ function welcome() {
 
 // play function
 
-function play(question, answer) {
+function play(question, answerList, answerIndex,i) {
 
-  var usrAns = readlineSync.question(question)
+  var usrAnsIndex = readlineSync.keyInSelect(answerList, question, { cancel: false })
 
-  if (usrAns.toUpperCase() === answer.toUpperCase()) {
-    console.log("")
-    console.log("Right!")
+  if (usrAnsIndex === answerIndex) {
+    console.log(chalk.green("Right!"))
 
     score++
   } else {
-    console.log("")
-    console.log("Wrong!")
-    console.log("Correct Answer: " + answer)
+    console.log(chalk.red("Wrong!"))
+    console.log("Correct Answer: " + chalk.green(questions[i].answerList[answerIndex]))
   }
   console.log("Current score: " + score)
   console.log("----------------------------------------")
@@ -72,16 +96,35 @@ function play(question, answer) {
 
 function game() {
   for (i = 0; i < questions.length; i++) {
-    play(questions[i].question, questions[i].answer)
+    play(questions[i].question, questions[i].answerList, answersIndex[i],i)
   }
 }
 
 function showScores() {
-  console.log("Congratulations, your final score is: " + score)
-  console.log("High scores are as follows. Send a screenshot if you have broken one.")
-  highScore.map(i => console.log(i.name + " : " + i.score))
+  console.log("Your final score is: " + chalk.green(score))
+  console.log("High scores are as follows: ")
+  highScore.map(i => console.log(chalk.blue(i.name) + " : " + chalk.green(i.score)))
 }
 
 welcome();
 game();
-showScores();
+
+var scoreList = Object.values(highScore)
+
+for (i = 0; i < scoreList.length; i++) {
+  if (score >= scoreList[i].score) {
+    console.log(chalk.green("Congratulations, you have beaten a high score."))
+    console.log()
+    highScore.splice(i, 0, { name: usrName, score: score })
+    showScores()
+    break
+  } else {
+    if (i === scoreList.length - 1) {
+      console.log(chalk.red("Sorry you couldn't beat a high score. Better luck next time!"))
+      showScores()
+    } else {
+      continue
+    }
+
+  }
+}
